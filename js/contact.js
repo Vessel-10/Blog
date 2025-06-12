a0cd69405b75ee17ad1debf6de0ada9259c13f1d
vessel <magomboanaclet@gmail.com>
2025-06-12 11:54:15 +0200

    additional js

--- js/contact.js
+++ js/contact.js
@@ -1,2 +1,14 @@
 const contact = document.getElementById('contact');
-console.log("hello world");
+contact.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const fullName = document.getElementById('name').value.trim()
+    const emailAddress = document.getElementById('email').value.trim()
+    const msg = document.getElementById("message").value.trim()
+
+    if(fullName === "" || emailAddress === "" || msg === ""){
+        alert("Please fill in your name, email address and the message")
+    }else{
+        alert("Thanks")
+    }
+})
